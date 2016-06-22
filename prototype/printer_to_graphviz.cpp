9fb91c46c6b13aa1aa1329370f809b2b0aa2fdbc
h3nnn4n <uber.renan@gmail.com>
2016-06-22 15:29:10 -0300

    Less sucky graphviz output

--- prototype/printer_to_graphviz.cpp
+++ prototype/printer_to_graphviz.cpp
@@ -4,8 +4,14 @@
 #include "types.h"
 
 void print_to_graphviz(_csp *csp){
-    std::cout << "digraph world {"
-                 "size=\"7,7\";";
+    std::cout << "digraph world {\n"
+                 //"edge[weight=0.1];"
+                 "nodesep=0.1;"
+                 "splines=compound;"
+                 "concentrate=true;"
+                 //"overlap_shrink=true;"
+                 //"sep=\"+25,25\";"
+                 "overlap=ortho;";
 
     for (int i = 0; i < (int)csp->graph.size(); ++i) {
         for (int j = 0; j < (int)csp->graph[i].size(); ++j) {
