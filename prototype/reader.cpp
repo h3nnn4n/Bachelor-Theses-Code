fd2741c0060624cbed4207fc1f2a18ec60e59e50
h3nnn4n <uber.renan@gmail.com>
2016-06-22 13:44:43 -0300

    Added header explaining file format + renamed csp50 to csp

--- prototype/reader.cpp
+++ prototype/reader.cpp
@@ -1,3 +1,39 @@
+/* OR-Library
+ * J E Beasley
+ *
+ * OR-Library is a collection of test data sets for a variety of OR problems.
+ *
+ * A full list of the test data sets available in OR-Library can be found here: http://people.brunel.ac.uk/~mastjjb/jeb/info.html
+ *
+ * Crew scheduling
+ *
+ * There are currently 10 data files.
+ *
+ * These data files are the ten test problems from J.E.Beasley
+ * and B.Cao "A tree search algorithm for the crew scheduling
+ * problem" European Journal of Operational Research 94 (1996)
+ * pp517-526.
+ *
+ * The test problems solved in that paper are available in
+ * the files csp50, csp100, csp150, csp200, csp250, csp300,
+ * csp350, csp400, csp450 and csp500 (where the number for
+ * each csp file is the number of tasks).
+ *
+ * The format of these data files is:
+ * number of tasks (N), time limit
+ * for each task i (i=1,...,N): start time, finish time
+ * for each transition arc between two tasks (i and j):
+ * i, j, cost of transition from i to j
+ *
+ * The value of the optimal solution for each of these data files
+ * for a varying number of crews is given in the above paper.
+ *
+ * The largest file is csp500 of size 250Kb (approximately).
+ * The entire set of files is of size 900Kb (approximately).
+ *
+ * Click here to access these files: http://people.brunel.ac.uk/~mastjjb/jeb/orlib/files
+ *
+ */
 #include <vector>
 #include <string>
 #include <iostream>
@@ -9,14 +45,14 @@
 _csp file_reader(std::string name){
     std::ifstream f(name);
 
-    _csp csp50;
+    _csp csp;
 
-    f >> csp50.N;
-    f >> csp50.time_limit;
+    f >> csp.N;
+    f >> csp.time_limit;
 
-    csp50.graph.resize(csp50.N + 1);
+    csp.graph.resize(csp.N + 1);
 
-    for (int i = 0; i < csp50.N; ++i) {
+    for (int i = 0; i < csp.N; ++i) {
         int start_time, end_time;
         f >> start_time;
         f >> end_time;
@@ -24,7 +60,7 @@ _csp file_reader(std::string name){
         t.id         = i;
         t.start_time = start_time;
         t.end_time   = end_time;
-        csp50.task.push_back(t);
+        csp.task.push_back(t);
     }
 
     while ( f ) {
@@ -34,8 +70,8 @@ _csp file_reader(std::string name){
         f >> e.dest;
         f >> e.cost;
         std::cout << i << '\n';
-        csp50.graph[i].push_back(e);
+        csp.graph[i].push_back(e);
     }
 
-    return csp50;
+    return csp;
 }
