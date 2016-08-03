a2fb9c7c2dfc0246d81f027a9d980250dbbb594b
h3nnn4n <uber.renan@gmail.com>
2016-08-03 16:06:55 -0300

    Fixed linking and removed old GLPK stuff

--- simple_soplex_prototype/main.cpp
+++ simple_soplex_prototype/main.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <cstdlib>
 
-#include <glpk.h>
+#include <soplex.h>
 
 #include "reader.h"
 #include "types.h"
@@ -10,13 +10,17 @@
 #include "backtrack.h"
 #include "utils.h"
 
+//using namespace soplex;
+
 int main(void) {
-    glp_prob *lp;
-    glp_iocp parm_mip;
-    glp_smcp parm_spx;
+    //glp_prob *lp;
+    //glp_iocp parm_mip;
+    //glp_smcp parm_spx;
+    //SoPlex mysoplex;
+    soplex::SoPlex mysoplex;
 
     bool max_journey = false;
-    int total_journeys = 0;
+    //int total_journeys = 0;
 
     std::vector<int>    ia;
     std::vector<int>    ja;
@@ -50,29 +54,29 @@ int main(void) {
     printf("\n");
     //return 0;
 
-    lp = glp_create_prob();
-    glp_set_prob_name(lp, "tsp");
-    glp_set_obj_dir(lp, GLP_MIN);
+    //lp = glp_create_prob();
+    //glp_set_prob_name(lp, "tsp");
+    //glp_set_obj_dir(lp, GLP_MIN);
 
-    glp_init_iocp(&parm_mip);
+    //glp_init_iocp(&parm_mip);
 
-    parm_mip.msg_lev    = GLP_MSG_OFF;      // Output level
+    //parm_mip.msg_lev    = GLP_MSG_OFF;      // Output level
 
-    glp_init_smcp(&parm_spx);
+    //glp_init_smcp(&parm_spx);
 
-    parm_spx.msg_lev = GLP_MSG_ALL;
+    //parm_spx.msg_lev = GLP_MSG_ALL;
 
-    glp_add_cols(lp, (int) journeys.size());
+    //glp_add_cols(lp, (int) journeys.size());
 
     for (int i = 1; i <= (int) journeys.size(); ++i) {  // Cada variavel esta associada a uma jornada
-        glp_set_col_bnds(lp, i, GLP_DB, 0.0, 1.0);     // 0 <= x_j <= 1 \forall j \in J
-        glp_set_col_kind(lp, i, GLP_IV);               // x_j é inteiro
-        glp_set_obj_coef(lp, i, journeys[i].cost);
+        //glp_set_col_bnds(lp, i, GLP_DB, 0.0, 1.0);     // 0 <= x_j <= 1 \forall j \in J
+        //glp_set_col_kind(lp, i, GLP_IV);               // x_j é inteiro
+        //glp_set_obj_coef(lp, i, journeys[i].cost);
     }
 
     for (int i = 0; i < t.N; ++i) {
-        int new_row = glp_add_rows(lp, 1);
-        glp_set_row_bnds(lp, new_row, GLP_FX, 1.0, 1.0); // Cada tarefa deve ser coberta exatamente uma vez
+        //int new_row = glp_add_rows(lp, 1);
+        //glp_set_row_bnds(lp, new_row, GLP_FX, 1.0, 1.0); // Cada tarefa deve ser coberta exatamente uma vez
     }
 
     for (int i = 0; i < (int) journeys.size(); ++i) {  // Cada variavel esta associada a uma jornada
@@ -90,39 +94,39 @@ int main(void) {
 
 
     if ( max_journey ) {
-        int new_row = glp_add_rows(lp, 1);
+        //int new_row = glp_add_rows(lp, 1);
         for (int i = 1; i <= t.N; ++i) {
-            ia.push_back(new_row );
+            //ia.push_back(new_row );
             ja.push_back(i       );
             ar.push_back(1       );
         }
-        glp_set_row_bnds(lp, t.N, GLP_DB, 04.0, 30.0); // Necessita de exatamente X jornadas
+        //glp_set_row_bnds(lp, t.N, GLP_DB, 04.0, 30.0); // Necessita de exatamente X jornadas
     }
 
-    glp_load_matrix(lp, (int) ia.size()-1, &ia[0], &ja[0], &ar[0]);
+    //glp_load_matrix(lp, (int) ia.size()-1, &ia[0], &ja[0], &ar[0]);
 
-    glp_simplex(lp, &parm_spx);
+    //glp_simplex(lp, &parm_spx);
 
-    for (int i = 1; i <= (int) journeys.size(); ++i) {
-        if ( glp_get_col_prim(lp, i) > 0.001 ) {
-            total_journeys++;
-        }
-    }
+    //for (int i = 1; i <= (int) journeys.size(); ++i) {
+        //if ( glp_get_col_prim(lp, i) > 0.001 ) {
+            //total_journeys++;
+        //}
+    //}
 
-    printf("Problem has\n%d columns\n%d rows\n", (int)journeys.size(), t.N);
-    printf("Found %d possible journeys\n", (int) journeys.size());
-    printf("Optimal solution is: %.3f with %d journeys\n", glp_get_obj_val(lp), total_journeys);
+    //printf("Problem has\n%d columns\n%d rows\n", (int)journeys.size(), t.N);
+    //printf("Found %d possible journeys\n", (int) journeys.size());
+    //printf("Optimal solution is: %.3f with %d journeys\n", glp_get_obj_val(lp), total_journeys);
 
-    for (int i = 1; i <= (int) journeys.size(); ++i) {
-        if ( glp_get_col_prim(lp, i) > 0.001 ) {
-            printf("%.2f: ", glp_get_col_prim(lp, i));
-            for (int j = 0; j < (int) journeys[i-1].covered.size(); ++j) {
-                printf("%d ", journeys[i-1].covered[j]);
-            }
-            printf("\n");
-        }
-        //printf("%.3f ", glp_get_col_prim(lp, i));
-    }
+    //for (int i = 1; i <= (int) journeys.size(); ++i) {
+        //if ( glp_get_col_prim(lp, i) > 0.001 ) {
+            //printf("%.2f: ", glp_get_col_prim(lp, i));
+            //for (int j = 0; j < (int) journeys[i-1].covered.size(); ++j) {
+                //printf("%d ", journeys[i-1].covered[j]);
+            //}
+            //printf("\n");
+        //}
+        ////printf("%.3f ", glp_get_col_prim(lp, i));
+    //}
     printf("\n");
 
     //std::cout << "Found " << glp_get_obj_val(lp) << " tasks\n";
