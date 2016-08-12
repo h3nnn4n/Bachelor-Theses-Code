b93053d168f0ca164d182796b927a6e6f140f7b8
h3nnn4n <uber.renan@gmail.com>
2016-08-12 08:57:16 -0300

    Wip

--- simple_cplex_prototype/main.cpp
+++ simple_cplex_prototype/main.cpp
@@ -16,38 +16,32 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    int    nfoods;
-    int    nnutr;
     int    status;
-    double *cost     = NULL;
-    double *lb       = NULL;
-    double *ub       = NULL;
-    double *nutrmin  = NULL;
-    double *nutrmax  = NULL;
-    double **nutrper = NULL;
+    //int    used_journeys = 0;
+    int    non_zero      = 0;
+    double *obj          = NULL;
+    double *lb           = NULL;
+    double *ub           = NULL;
+    double *rhs          = NULL;
+    char   *sense        = NULL;
+    int    *rmatbeg      = NULL;
+    int    *rmatind      = NULL;
+    double *rmatval      = NULL;
+
+    char input_name[256];
+    char output_name[256];
 
     CPXENVptr     env = NULL;
     CPXLPptr      lp = NULL;
 
-    env = CPXopenCPLEX (&status);
+    env    = CPXopenCPLEX   ( &status                               ) ;
+    status = CPXsetintparam ( env, CPXPARAM_ScreenOutput  , CPX_ON  ) ;
+    status = CPXsetintparam ( env, CPXPARAM_Read_DataCheck, CPX_ON  ) ;
+    lp     = CPXcreateprob  ( env, &status                , "spp"   ) ;
+    status = CPXchgobjsen   ( env, lp                     , CPX_MIN ) ;
 
-    if ( env == NULL ) {
-        char  errmsg[CPXMESSAGEBUFSIZE];
-        fprintf (stderr, "Could not open CPLEX environment.\n");
-        CPXgeterrorstring (env, status, errmsg);
-        fprintf (stderr, "%s", errmsg);
-        return EXIT_FAILURE;
-    }
-    return EXIT_SUCCESS;
-
-    //soplex::SoPlex mysoplex;
-    char input_name[256];
-    char output_name[256];
-
-    int used_journeys = 0;
-
-    sprintf(input_name,  "%s.txt", argv[1]);
-    sprintf(output_name, "%s.lp",  argv[1]);
+    sprintf(input_name , "%s.txt", argv[1]);
+    sprintf(output_name, "%s.lp" , argv[1]);
 
     _csp t = file_reader(input_name);
     std::vector<_journey> journeys;
@@ -65,46 +59,63 @@ int main(int argc, char *argv[]) {
         backtrack(t, i, 0, 0, vec, journeys);
     }
 
-    //print_graph(t);
-    //printf("\n");
-    //print_journeys(journeys);
-    //printf("\n");
+    print_graph(t);
+    printf("\n");
+    print_journeys(journeys);
+    printf("\n");
     //return 0;
 
-    //mysoplex.setIntParam(soplex::SoPlex::OBJSENSE, soplex::SoPlex::OBJSENSE_MINIMIZE);
-    //soplex::DSVector dummycol((int) journeys.size());
+    rhs     = (double*) malloc ( sizeof(double) * t.N                   );
+    obj     = (double*) malloc ( sizeof(double) * (int) journeys.size() );
+    lb      = (double*) malloc ( sizeof(double) * (int) journeys.size() );
+    ub      = (double*) malloc ( sizeof(double) * (int) journeys.size() );
+    sense   = (char  *) malloc ( sizeof(char  ) * t.N                   );
+    rmatbeg = (int   *) malloc ( sizeof(int   ) * t.N                   );
 
-    for (int i = 0; i < (int) journeys.size(); ++i) {
-        //mysoplex.addColReal(soplex::LPCol(journeys[i].cost, dummycol, 1.0, 0.0));
+    for (int i = 0; i < t.N; ++i) {
+        rhs  [i] = 1.0;
+        sense[i] = 'E';
     }
 
-    //std::vector<soplex::DSVector> rows;
-    //rows.resize( t.N + 1 );
-    for (int i = 0; i < (int) journeys.size(); ++i) {
+    for (int i = 0; i < (int) journeys.size(); ++i) { // Contagem de não-zeros
+        lb [i] = 0.0;
+        ub [i] = 1.0;
+        obj[i] = journeys[i].cost;
         for (int j = 0; j < (int) journeys[i].covered.size(); ++j) {
-            //rows[journeys[i].covered[j]].add(i, 1);
+            non_zero++;
         }
     }
 
-    //soplex::DSVector row1;
-    for (int i = 0; i < (int) journeys.size(); ++i) {
-        //row1.add(i, 1);
+    rmatind = (int*   ) malloc (sizeof(int)    * non_zero);
+    rmatval = (double*) malloc (sizeof(double) * non_zero);
+
+    for (int i = 0, k = 0; i < (int) journeys.size(); ++i) {  // Cada variavel esta associada a uma jornada
+        if ( i ) {
+            rmatbeg[i] = (int) journeys[i].covered.size();
+            rmatbeg[i] += rmatbeg[i-1];
+        } else {
+            rmatbeg[i] = 0;
+        }
+
+        for (int j = 0; j < (int) journeys[i].covered.size(); ++j) {
+            rmatind[k] = j;
+            rmatval[k] = 1;
+            printf("i:%3d\tk: %3d \t%5d\t%5d\t%5.2f\n", i, k, rmatbeg[i], rmatind[k], rmatval[k]);
+            k++;
+        }
+        printf("\n");
     }
 
-    for (int i = 1; i <= t.N; ++i) {
-        //mysoplex.addRowReal(soplex::LPRow(1.0, rows[i], soplex::infinity));
-        //mysoplex.addRowReal(soplex::LPRow(1.0, rows[i], 1.0));
-    }
+    status = CPXnewcols (env, lp, (int) journeys.size(), obj, lb, ub, NULL, NULL);
+    //printf("STATUS = %d\n", status);
+    status = CPXaddrows (env, lp, (int) journeys.size(), t.N, non_zero, rhs, sense,
+                         rmatbeg, rmatind, rmatval,
+                         NULL, NULL);
 
-    //int p = 20;
-    //mysoplex.addRowReal(soplex::LPRow(p, row1, p));
+    //printf("STATUS = %d\n", status);
 
-    //mysoplex.writeFileReal(output_name, NULL, NULL, NULL);
-
-    //soplex::SPxSolver::Status stat;
-    //soplex::DVector prim((int) journeys.size());
-    //soplex::DVector dual(t.N);
-    //stat = mysoplex.solve();
+    status = CPXlpopt (env, lp);
+    status = CPXwriteprob (env, lp, output_name, NULL);
 
     printf("\n");
     printf("N: \t %6d \t maxt: %4d\n", t.N, t.time_limit);
