a13eee47cc55bb750c677c5c5a99f72f54c157ec
h3nnn4n <uber.renan@gmail.com>
2016-12-15 11:49:27 -0200

    Wip on using expr

--- concert_cplex_prototype/concert.cpp
+++ concert_cplex_prototype/concert.cpp
@@ -27,8 +27,7 @@ int main (int argc, char **argv) {
 
         if ( argc != 2 ) {
             printf("Missing argument\n");
-            return EXIT_FAILURE;
-        }
+            return EXIT_FAILURE; }
 
         sprintf(input_name , "%s.txt", argv[1]);
         sprintf(output_name, "%s.lp" , argv[1]);
@@ -36,11 +35,14 @@ int main (int argc, char **argv) {
         _csp t = file_reader(input_name);
         std::vector<_journey> journeys;
 
-        //srand(time(NULL));
-        srand(666);
+        srand(time(NULL));
+        //srand(666);
 
         journeys.push_back(all_powerful_journey(&t));
-        journeys.push_back(random_journey(&t));
+
+        for (int i = 0; i < 5; ++i) {
+            journeys.push_back(random_journey(&t));
+        }
 
         print_journeys(journeys);
         printf("\n");
@@ -55,23 +57,35 @@ int main (int argc, char **argv) {
         IloCplex cplex(model);
         cplex.exportModel("lpex1.lp");
 
-        // Optimize the problem and obtain solution.
+        // Optimize the problem and obtain solution for the first run.
         if ( !cplex.solve() ) {
             env.error() << "Failed to optimize LP" << endl;
             throw(-1);
         }
 
-        IloNumArray vals(env);
-        env.out() << "Solution status = " << cplex.getStatus() << endl;
-        env.out() << "Solution value  = " << cplex.getObjValue() << endl;
-        cplex.getValues(vals, var);
-        env.out() << "Values        = " << vals << endl;
-        cplex.getSlacks(vals, con);
-        env.out() << "Slacks        = " << vals << endl;
-        cplex.getDuals(vals, con);
-        env.out() << "Duals         = " << vals << endl;
-        cplex.getReducedCosts(vals, var);
-        env.out() << "Reduced Costs = " << vals << endl;
+        // Solves the subproblem for the first time to get the
+        // reduced cost and begin the column generation process
+        IloNumArray red_cost(env);
+        IloNumArray duals(env);
+        cplex.getReducedCosts(red_cost, var);
+        cplex.getDuals(duals, con);
+        subproblem(red_cost, duals, &t, journeys);
+
+        //while ( 1 ) {
+
+        //}
+
+        //IloNumArray vals(env);
+        //env.out() << "Solution status = " << cplex.getStatus() << endl;
+        //env.out() << "Solution value  = " << cplex.getObjValue() << endl;
+        //cplex.getValues(vals, var);
+        //env.out() << "Values        = " << vals << endl;
+        //cplex.getSlacks(vals, con);
+        //env.out() << "Slacks        = " << vals << endl;
+        //cplex.getDuals(vals, con);
+        //env.out() << "Duals         = " << vals << endl;
+        //cplex.getReducedCosts(vals, var);
+        //env.out() << "Reduced Costs = " << vals << endl;
     }
     catch (IloException& e) {
         cerr << "Concert exception caught: " << e << endl;
