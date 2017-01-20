a3f718359b932942c5efb59a260cf6be0c840783
h3nnn4n <uber.renan@gmail.com>
2017-01-20 12:32:54 -0200

    Main file BUMP, to keep up with the other files

--- concert_cplex_prototype/concert.cpp
+++ concert_cplex_prototype/concert.cpp
@@ -25,9 +25,10 @@ int main (int argc, char **argv) {
         char input_name [256];
         char output_name[256];
 
-        if ( argc != 2 ) {
+        if ( argc == 1 ) {
             printf("Missing argument\n");
-            return EXIT_FAILURE; }
+            return EXIT_FAILURE;
+        }
 
         sprintf(input_name , "%s.txt", argv[1]);
         sprintf(output_name, "%s.lp" , argv[1]);
@@ -35,18 +36,35 @@ int main (int argc, char **argv) {
         _csp t = file_reader(input_name);
         std::vector<_journey> journeys;
 
+        if ( argc == 3 ) {
+            t.n_journeys = atoi(argv[2]);
+        } else {
+            fprintf(stderr, "Assuming the number of journeys is equal to the number of tasks!\n");
+            t.n_journeys = t.N;
+        }
+
         srand(time(NULL));
         //srand(666);
 
-        journeys.push_back(all_powerful_journey(&t));
+        //// Prints the graph (it indexes from zero)
+        //for (int i = 0; i < (int)t.graph.size(); ++i) {
+            //printf("%3d -> ", i);
+            //for (int j = 0; j < (int)t.graph[i].size(); ++j) {
+                //printf("%3d ", t.graph[i][j].dest);
+            //}
+            //printf("\n");
+        //}
+
+        build_heur_sol ( &t, journeys );
+        //journeys.push_back(all_powerful_journey(&t));
 
         //for (int i = 0; i < 5; ++i) {
             //journeys.push_back(random_journey(&t));
         //}
 
-        //print_journeys(journeys);
-        //printf("\n");
-        //return 0;
+        print_journeys(journeys);
+        printf("\n");
+        return 0;
 
         // begins model construction
         IloNumVarArray var(env);
@@ -55,18 +73,14 @@ int main (int argc, char **argv) {
         populate_model(model, var, con, &t, journeys) ;
 
         IloCplex cplex(model);
-        cplex.exportModel("lpex1.lp");
+        cplex.exportModel("lpex0.lp");
 
+        cplex.setOut(env.getNullStream());
         // Optimize the problem and obtain solution for the first run.
         if ( !cplex.solve() ) {
             env.error() << "Failed to optimize LP" << endl;
             throw(-1);
         }
-
-        // Solves the subproblem for the first time to get the
-        // reduced cost and begin the column generation process
-        IloNumArray red_cost(env);
-        IloNumArray duals(env);
 
         //IloNumArray vals(env);
         //env.out() << "Solution status = " << cplex.getStatus() << endl;
@@ -80,14 +94,83 @@ int main (int argc, char **argv) {
         //cplex.getReducedCosts(vals, var);
         //env.out() << "Reduced Costs = " << vals << endl;
         //cout << "\n\n";
+        //
 
-        cplex.getReducedCosts(red_cost, var);
-        cplex.getDuals(duals, con);
-        subproblem(red_cost, duals, &t, journeys);
+        double reduced_cost;
 
-        //while ( 1 ) {
+        int cont = 0;
 
-        //}
+        do {
+            // Solves the master problem
+            if ( !cplex.solve() ) {
+                env.error() << "Failed to optimize LP" << endl;
+                throw(-1);
+            }
+
+            // Solves the subproblem
+            IloNumArray red_cost(env);
+            IloNumArray duals(env);
+            cplex.getReducedCosts(red_cost, var);
+            cplex.getDuals(duals, con);
+
+            _journey new_journey = subproblem(red_cost, duals, &t, journeys, &reduced_cost);
+
+            journeys.push_back(new_journey);
+
+            // Gets a reference to the objective function and adds the new coef for the column
+            //IloObjective obj = IloMinimize(env);
+            IloObjective obj = IloMinimize(env);
+
+            // Create a new expression to build the new column
+            //puts("a");
+            //IloNumColumn col;
+            IloNumColumn col = obj(new_journey.cost);
+            printf(" cost = %d\n", new_journey.cost);
+            //IloNumExpr expr(env);
+            //obj.add(obj(new_journey.cost));
+
+            //puts("0");
+            //col += obj(new_journey.cost);
+            //puts("1");
+
+            // Walks throught the new journey and builds the coeffs for the column
+            for (int i = 0; i < (int)new_journey.covered.size(); ++i) {
+                //expr +=
+                printf("%d %d\n", i, new_journey.covered[i]);
+                col += con[new_journey.covered[i]](1.0);
+            }
+
+            col += con[t.N](1.0);
+
+            //model.add(expr <= t->time_limit);
+            //var.add(IloNumVar(end, 0.0, 1.0, ILOFLOAT));
+            //var.add(IloNumVar( expr ));
+
+            var.add(IloNumVar(obj(new_journey.cost) + col, 0.0, 1.0, ILOFLOAT));
+            //obj.setLinearCoef(var[(int)journeys.size()], new_journey.cost);
+            model.add(var);
+            //model.add(obj);
+            //puts("3");
+            //expr.end();
+
+            //if ( cont++ > 20 ) reduced_cost = 0;
+
+            //switch ( cont ) {
+                //case 1:
+                    //cplex.exportModel("lpex1.lp");
+                    //break;
+                //case 2:
+                    //cplex.exportModel("lpex2.lp");
+                    //break;
+                //case 3:
+                    cplex.exportModel("lpex3.lp");
+                    //break;
+                //default:
+                    //break;
+            //}
+
+            printf(" - %d %f\n", cont, reduced_cost);
+        } while ( reduced_cost != 0 );
 
     }
     catch (IloException& e) {
